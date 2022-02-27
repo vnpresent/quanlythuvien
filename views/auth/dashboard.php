e3a9a11870bb6b3761ad8fbcf8f3f16e67583a11
xuan nguyen <xuanquy1120@gmail.com>
2022-02-27 22:00:58 +0700

    front-end-dashboard

--- views/auth/dashboard.php
+++ views/auth/dashboard.php
@@ -1,1 +1,81 @@
-<h1>Đại Học Thủy Lợi</h1>
+<?php
+if (Auth::isLogin()) {
+?>
+<div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <?php
+        include_once('./assets/layouts/siderbar.php');
+        ?>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
+                <div class="d-flex align-items-center">
+                    <h2 class="fs-2 m-0">Admin</h2>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+            <div class="container-fluid px-4">
+                <div class="row g-3 my-2">
+                    <div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">720</h3>
+                                <p class="fs-5">Tài liệu</p>
+                            </div>
+                    
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">4920</h3>
+                                <p class="fs-5">Tài liệu</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">3899</h3>
+                                <p class="fs-5">Tài liệu</p>
+                            </div>
+                           
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">%25</h3>
+                                <p class="fs-5">Tài liệu</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+    </div>
+<?php
+}
+else
+{
+?>
+<a href="index.php?controller=auth&action=login" class="nav-link active text-dark">Đăng nhập</a>
+<?php
+}
+?>
